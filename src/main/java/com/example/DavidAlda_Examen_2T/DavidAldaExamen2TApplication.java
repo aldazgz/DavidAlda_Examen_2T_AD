0002e8225b78e4c5021db1d75960a60b76152e7a
David Alda <aldazgz95@gmail.com>
2025-03-07 21:46:50 +0100

    Commit 2

--- src/main/java/com/example/DavidAlda_Examen_2T/DavidAldaExamen2TApplication.java
+++ src/main/java/com/example/DavidAlda_Examen_2T/DavidAldaExamen2TApplication.java
@@ -3,7 +3,7 @@ package com.example.DavidAlda_Examen_2T;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Repositorio GitHub del proyecto:
+// Repositorio GitHub del proyecto: https://github.com/aldazgz/DavidAlda_Examen_2T_AD
 
 @SpringBootApplication
 public class DavidAldaExamen2TApplication {
